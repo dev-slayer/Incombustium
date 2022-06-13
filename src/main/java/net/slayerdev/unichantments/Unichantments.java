7a9ec7fdbc96b9b3ccd475f17d2d2f7c9e01ad66
SlayerDev <anthonywebster@outlook.com>
2022-06-13 18:06:35 -0400

    Initial commit

---
+++ src/main/java/net/slayerdev/unichantments/Unichantments.java
@@ -0,0 +1,15 @@
+package net.slayerdev.unichantments;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Unichantments implements ModInitializer {
+	public static final String MOD_ID = "unichantments";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/slayerdev/unichantments/UnichantmentsClient.java
@@ -0,0 +1,10 @@
+package net.slayerdev.unichantments;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UnichantmentsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
